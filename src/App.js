bd2b2504d436bb379a446552ebd1e964f20c948f
TheSillyP <Petjenorden@gmail.com>
2024-07-29 11:16:26 +0200

    Fixed routing problem

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 
 const App = () => {
   return (
-    <Router>
       <div>
         <Routes>
-          <Route exact path="/" component={HomePage} />
+          <Route exact path="/" element={<HomePage />} />
           {/* Add more routes as needed */}
         </Routes>
       </div>
-    </Router>
   );
 };
 
